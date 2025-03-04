b69b1ef64a2390e343c799fcb7fe32d5cbf284fd
MADHU T <MADHU T@LAPTOP-NUAUSDAK>
2025-03-04 11:08:31 +0530

    Madhu

---
+++ Automation/printAllAutoSelection.java
@@ -0,0 +1,30 @@
+package QSP;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class printAllAutoSelection
+{
+	public static void main(String []args) throws InterruptedException
+	{
+WebDriver driver=new ChromeDriver();//open the browzer
+driver.get("https://www.google.com/");//enter the url
+driver.findElement(By.id("APjFqb")).sendKeys("java");//type java in text box
+Thread.sleep(2000);
+List<WebElement>allsugg=driver.findElements(By.xpath("//span[contains(text(),'java')]"));//capture all auto suggestion
+		int count=allsugg.size();//print the count of all auto suggestion
+		System.out.println(count);
+		for(int i=0;i<count;i++)//print all text of auto suggestion
+		{
+			String text=allsugg.get(i).getText();
+			System.out.println(text);
+		}
+		allsugg.get(0).click();//select first auto suggestion
+		}
+
+
+}
