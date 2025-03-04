c21401a44fafc820b925d2d661d775436eaf0944
MADHU T <MADHU T@LAPTOP-NUAUSDAK>
2025-03-04 11:25:18 +0530

    madhu

--- Automation/src/MadhuAutomation/printSeleniumSearchs.java
+++ Automation/src/MadhuAutomation/printSeleniumSearchs.java
@@ -1,4 +1,4 @@
-package QSP;
+package MadhuAutomation;
 
 import java.util.List;
 
@@ -7,15 +7,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class printAllAutoSelection
+public class printSeleniumSearchs
 {
 	public static void main(String []args) throws InterruptedException
 	{
 WebDriver driver=new ChromeDriver();//open the browzer
 driver.get("https://www.google.com/");//enter the url
-driver.findElement(By.id("APjFqb")).sendKeys("java");//type java in text box
+driver.findElement(By.id("APjFqb")).sendKeys("Selenium");//type java in text box
 Thread.sleep(2000);
-List<WebElement>allsugg=driver.findElements(By.xpath("//span[contains(text(),'java')]"));//capture all auto suggestion
+List<WebElement>allsugg=driver.findElements(By.xpath("//span[contains(text(),'selenium')]"));//capture all auto suggestion
 		int count=allsugg.size();//print the count of all auto suggestion
 		System.out.println(count);
 		for(int i=0;i<count;i++)//print all text of auto suggestion
@@ -23,7 +23,7 @@ List<WebElement>allsugg=driver.findElements(By.xpath("//span[contains(text(),'ja
 			String text=allsugg.get(i).getText();
 			System.out.println(text);
 		}
-		allsugg.get(0).click();//select first auto suggestion
+		allsugg.get(count-1).click();//select first auto suggestion
 		}
 
 
