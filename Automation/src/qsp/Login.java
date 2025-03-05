3202a015cd08bd04bd20c4322384f69889e860b1
BHARATH T M <BHARATH T M@LAPTOP-KR6AR41B>
2025-03-05 10:25:55 +0530

    Merge branch 'master' of https://github.com/Keerthi562/Actitime.git

--- Automation/src/qsp/Login.java
+++ Automation/src/qsp/Login.java
@@ -1,4 +1,4 @@
-package DayJan24;
+package qsp;
 
 import java.awt.AWTException;
 import java.awt.Robot;
@@ -8,7 +8,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class bharathTM  {
+public class Login {
 
 	public static void main(String[] args) throws AWTException, InterruptedException {
     WebDriver driver=new ChromeDriver();
