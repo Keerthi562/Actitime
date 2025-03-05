99406e19d8c5e29f2dfa6ade8916e213f6ff6278
fayaz <fayaz@SHAIK_FAYAZ>
2025-03-05 10:05:54 +0530

    commited by fayaz

---
+++ Automation/src/DP/LearnDP.java
@@ -0,0 +1,13 @@
+package DP;
+
+import org.testng.annotations.DataProvider;
+
+public class LearnDP {
+	@DataProvider
+	public Object[][] getData() {
+		 Object[][]data= {{"admin","manager"},{"fayaz","shaik"},{"honey","kerala"},{"ranju","late"}
+		,{"vineeth","busy"},{"deepak","experience"},{"sandeep","expensive"}};
+		return data;
+	}
+
+}
